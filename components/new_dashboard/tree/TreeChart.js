7dbe31aa183ac7a8ebd4fc271a815dc551d2d629
isiagi <isiagigeofrey0@gmail.com>
2023-08-16 15:03:06 +0300

    trying2

--- components/new_dashboard/tree/TreeChart.js
+++ components/new_dashboard/tree/TreeChart.js
@@ -1,18 +1,20 @@
-"use client"
+"use client";
 
 import React from "react";
 import { Tree, TreeNode } from "react-organizational-chart";
 
 function TreeChart() {
   return (
-    <Tree label={<div>Root</div>}>
-      <TreeNode label={<div>Child 1</div>}>
-        <TreeNode label={<div>Grand Child</div>} />
-      </TreeNode>
-      <TreeNode label={<div>Child 1</div>}>
-        <TreeNode label={<div>Grand Child</div>} />
-      </TreeNode>
-    </Tree>
+    <>
+      <Tree label={<div>Root</div>}>
+        <TreeNode label={<div>Child 1</div>}>
+          <TreeNode label={<div>Grand Child</div>} />
+        </TreeNode>
+        <TreeNode label={<div>Child 1</div>}>
+          <TreeNode label={<div>Grand Child</div>} />
+        </TreeNode>
+      </Tree>
+    </>
   );
 }
 
